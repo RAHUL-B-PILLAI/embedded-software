725c15f8ba753581b59931201b82abc10b9375e6
RAHUL-B-PILLAI <pillaibrahul@gmail.com>
2022-02-10 11:23:16 +0000

    first stage code

--- assignment-1/assignment_1/assignment_1.ino
+++ assignment-1/assignment_1/assignment_1.ino
@@ -1,8 +1,62 @@
+
+
+/* 
+ Calculating the parameter for signal A 
+ 
+ SURNAME IS PILLAI
+
+ So, P=16, I=9, L=12, A=1
+
+ a = P * 100 = 16 * 100 = 1600
+  
+ b = I * 100 = 9 * 100 = 900
+ 
+ c = L + 4 = 12 + 4 = 16
+
+ d = L * 500 = 12 * 500 = 6000
+ 
+ mode = rem(A/4)+1 =
+
+*/
+
+
+#define red_led 15
+#define green_led 21
+#define push_1 22
+#define push_2 23          //defining variables for gpio pin in esp32
+
+int a=1600, b=900, c=16, d=6000;                    //defining the parameter of singal A waveform
+
 void setup() {
-  // put your setup code here, to run once:
+  
+pinMode(red_led, OUTPUT);
+pinMode(green_led,OUTPUT);
+pinMode(push_1,INPUT);
+pinMode(push_2,INPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+// Red led is for signal B and green led is for signal A
+
+//starting Signal B for 50 microseconds
+
+digitalWrite(red_led,HIGH);
+delayMicroseconds(50);
+digitalWrite(red_led,LOW); 
+
+//starting Signal A
+
+for(int i=0;i<=c;i++){
+
+  digitalWrite(green_led,HIGH);
+  delayMicroseconds(a);
+  digitalWrite(green_led,LOW);
+  delayMicroseconds(b);
+
+}
+
 
 }
